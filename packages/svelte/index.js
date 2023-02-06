4f4c7ddf4585761f14ad04c8a7b6a991cc7d56ee
fazulk <jfasulkey@gmail.com>
2023-02-06 12:06:09 -0800

    feat: add svelte

---
+++ packages/svelte/index.js
@@ -0,0 +1,39 @@
+const { isPackageExists } = require('local-pkg')
+
+const TS = isPackageExists('typescript')
+
+if (!TS)
+  console.warn('[@antfu/eslint-config] TypeScript is not installed, fallback to JS only.')
+
+module.exports = {
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        ...(TS
+          ? { '@typescript-eslint/no-unused-vars': 'off' }
+          : null),
+      },
+    },
+  ],
+  extends: [
+    'plugin:svelte/recommended',
+    TS
+      ? '@antfu/eslint-config-ts'
+      : '@antfu/eslint-config-basic',
+  ],
+  rules: {
+    'svelte/indent': [
+      'error',
+      {
+        indentScript: false,
+      },
+    ],
+  },
+}
