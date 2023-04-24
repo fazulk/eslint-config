8917fd35b5734d2bc6a0559299bfd97e877dc7a6
fazulk <jfasulkey@gmail.com>
2023-04-24 11:25:41 -0700

    feat: added no auto fix

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -14,12 +14,6 @@ module.exports = {
     }],
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'vue/component-options-name-casing': ['error', 'PascalCase'],
-    'vue/custom-event-name-casing': ['error',
-      'kebab-case',
-      {
-        ignores: [],
-      },
-    ],
     'unused-imports/no-unused-imports': 'off',
     'no-autofix/unused-imports/no-unused-imports': 'error',
   },
